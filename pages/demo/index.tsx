b7008984d4cdb882cc7285d9b1acbd48b6de67a9
Vladimir Jimenez <allejo@me.com>
2021-02-15 20:37:16 -0800

    Add share button to Demo page

--- pages/demo/index.tsx
+++ pages/demo/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { CodeBlock } from "../../components/codeblock";
 import { DumbEditor } from "../../components/dumb-editor";
@@ -7,17 +7,63 @@ import { MainLayout } from "../../layouts/main";
 
 import styles from '../../styles/Demo.module.scss';
 
+interface Data {
+  code: string;
+  lang: string | null;
+}
+
+function persistToURL({ code, lang }: Data): void {
+  const params = new URLSearchParams(window.location.hash);
+
+  if (lang !== null) {
+    params.set('lang', lang);
+  }
+
+  params.set('code', btoa(code));
+
+  window.location.hash = params.toString();
+}
+
+function parseURL(): Data {
+  const params = new URLSearchParams(window.location.hash.substring(1));
+
+  return {
+    lang: params.get('lang'),
+    code: atob(params.get('code') ?? ''),
+  }
+}
+
 const Demo = () => {
   const [code, setCode] = useState('');
   const [lang, setLang] = useState<string | null>(null);
+
+  const handleShare = () => {
+    persistToURL({ code, lang });
+  };
+
+  useEffect(() => {
+    const { lang, code } = parseURL();
+
+    setCode(code);
+    setLang(lang);
+  }, []);
 
   return (
     <MainLayout>
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <div className="d-flex">
-              <LanguageSelector className="mb-3" onChange={setLang} />
+            <div className="d-flex mb-3">
+              <LanguageSelector onChange={setLang} />
+
+              <div className="ml-auto mt-auto">
+                <button
+                  className={styles.shareButton}
+                  onClick={handleShare}
+                >
+                  Share
+                </button>
+              </div>
             </div>
 
             <DumbEditor
