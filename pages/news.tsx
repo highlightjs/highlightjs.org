c08acd7fcfe56f96815308383ea64156e5073d14
Vladimir Jimenez <allejo@me.com>
2021-02-19 22:59:29 -0800

    Remove unnecessary braces on News page

--- pages/news.tsx
+++ pages/news.tsx
@@ -4,34 +4,32 @@ import Timestamp from '../components/timestamp';
 import { MainLayout } from '../layouts/main';
 import styles from '../styles/News.module.scss';
 
-const News = ({ releases }) => {
-  return (
-    <MainLayout>
-      <div className="container">
-        <h1>Project News</h1>
+const News = ({ releases }) => (
+  <MainLayout>
+    <div className="container">
+      <h1>Project News</h1>
 
-        <section>
-          {releases.map((release) => (
-            <article className={styles.newsArticle}>
-              <header className={styles.newsHeader}>
-                <h2 className="mb-0">
-                  <a href={release.url}>{release.name}</a>
-                </h2>
-                <div>
-                  <Timestamp timestamp={release.published_at} />
-                  <span className="mx-2">&middot;</span>
-                  <GitHubAvatar username={release.author.login} size={24} />
-                </div>
-              </header>
+      <section>
+        {releases.map((release) => (
+          <article className={styles.newsArticle}>
+            <header className={styles.newsHeader}>
+              <h2 className="mb-0">
+                <a href={release.url}>{release.name}</a>
+              </h2>
+              <div>
+                <Timestamp timestamp={release.published_at} />
+                <span className="mx-2">&middot;</span>
+                <GitHubAvatar username={release.author.login} size={24} />
+              </div>
+            </header>
 
-              <Markdown body={release.body} />
-            </article>
-          ))}
-        </section>
-      </div>
-    </MainLayout>
-  );
-};
+            <Markdown body={release.body} />
+          </article>
+        ))}
+      </section>
+    </div>
+  </MainLayout>
+);
 
 // This function gets called at build time
 export async function getStaticProps() {
