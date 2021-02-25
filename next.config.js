43214980842a96b2391def1c584b53c308403b87
Vladimir Jimenez <allejo@me.com>
2021-02-24 18:17:46 -0800

    Add some redirects for urls from the original website

--- next.config.js
+++ next.config.js
@@ -3,4 +3,23 @@ const withTM = require('next-transpile-modules')([
   'text-field-edit',
 ]);
 
-module.exports = withTM();
+module.exports = {
+  ...withTM(),
+  redirects: async () => [
+    {
+      source: '/usage',
+      destination: '/#usage',
+      permanent: false,
+    },
+    {
+      source: '/contribute',
+      destination: 'https://github.com/highlightjs/highlight.js',
+      permanent: true,
+    },
+    {
+      source: '/static/demo',
+      destination: '/examples',
+      permanent: true,
+    },
+  ],
+};
