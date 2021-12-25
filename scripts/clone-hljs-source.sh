d9a856ac93685e665e0e05e466729ef312db7107
Vladimir Jimenez <allejo@me.com>
2021-12-24 23:40:08 -0800

    Build hljs languages during build process

--- scripts/clone-hljs-source.sh
+++ scripts/clone-hljs-source.sh
@@ -1,22 +1,42 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p data/snippets/
-rm -rf highlight.js
+clean_data_dir() {
+  mkdir -p data/{downloads,snippets}/
+  rm -rf highlight.js
+}
 
-hljsVersion=$(cat package.json | jq -re '.dependencies["highlight.js"]')
+clone_hljs_repo() {
+  hljsVersion=$(cat package.json | jq -re '.dependencies["highlight.js"]')
 
-echo "Using out highlight.js $hljsVersion..."
-git config --global advice.detachedHead false
-git clone -b "$hljsVersion" https://github.com/highlightjs/highlight.js.git
+  echo "Using out highlight.js $hljsVersion..."
+  git config --global advice.detachedHead false
+  git clone -b "$hljsVersion" https://github.com/highlightjs/highlight.js.git
 
-cd highlight.js
-for folder in test/detect/*
-do
-  if [[ -f $folder ]]; then
-    continue
-  fi
+  cd highlight.js
+}
 
-  filename=$(basename $folder)
-  cp "$folder/default.txt" "../data/snippets/$filename.txt"
-done
+get_code_snippets() {
+  for folder in test/detect/*
+  do
+    if [[ -f $folder ]]; then
+      continue
+    fi
+
+    filename=$(basename $folder)
+    cp "$folder/default.txt" "../data/snippets/$filename.txt"
+  done
+}
+
+build_hljs_download() {
+  npm i
+  node tools/build.js -t cdn
+
+  cp build/languages/* "../data/downloads/"
+  cp build/highlight.min.js "../data/downloads/"
+}
+
+clean_data_dir
+clone_hljs_repo
+get_code_snippets
+build_hljs_download
