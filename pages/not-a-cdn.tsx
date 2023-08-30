5d61040a72a12a05528a6ca1ca76436250751315
Vladimir Jimenez <allejo@me.com>
2023-08-30 10:58:19 -0700

    Blackhole redirect for the former /static/

---
+++ pages/not-a-cdn.tsx
@@ -0,0 +1,11 @@
+import { MainLayout } from "../layouts/main";
+
+const NotACdn = () => (
+  <MainLayout title="We're not a CDN">
+    <div className="container">
+      <p className="mb-4">Do not use us a CDN. You should not be linking to any assets that are hosted on our website. We are getting million of garbage requests due to this misuse of our assets.</p>
+    </div>
+  </MainLayout>
+);
+
+export default NotACdn;
